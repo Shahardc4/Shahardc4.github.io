e370957f7eb9d0fa4d143fce199fd1986ecf99f1
Shahardc4 <74099020+Shahardc4@users.noreply.github.com>
2022-03-28 23:17:25 +0300

    Updates

---
+++ precache-manifest.48a3ee6f45601e80da259a6479fb63b6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d19a841a0daa9efe7aa5c366fb0498",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dc8415034bff84661b2",
+    "url": "/static/css/main.f3e7ae4d.chunk.css"
+  },
+  {
+    "revision": "630aa22eebfa1a497e70",
+    "url": "/static/js/2.265ec63d.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.265ec63d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dc8415034bff84661b2",
+    "url": "/static/js/main.972f2996.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "d09d8b278480ae3a731826d84f53ca16",
+    "url": "/static/media/IR Project Main.d09d8b27.png"
+  },
+  {
+    "revision": "15e38cfc3e6c0f071da976474b91ffcc",
+    "url": "/static/media/big indices runs.15e38cfc.jpg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "ded86812e1501ff74ee65bad4ab546de",
+    "url": "/static/media/download to the environment.ded86812.jpg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "7dc59d2293215b11def41078141d2049",
+    "url": "/static/media/inverted index representation.7dc59d22.jpg"
+  },
+  {
+    "revision": "dd5db277470ec9f1cf135786604793f0",
+    "url": "/static/media/returning and ranking.dd5db277.jpg"
+  },
+  {
+    "revision": "32b85c244e601b93713315abb0287fa3",
+    "url": "/static/media/small indices runs.32b85c24.jpg"
+  },
+  {
+    "revision": "d0b35981d25199a9c3b1b070f4ad135c",
+    "url": "/static/media/uploading.d0b35981.jpg"
+  }
+]);
